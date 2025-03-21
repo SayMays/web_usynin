b00ff0dc46794463323998c47b840f46f8d4e438
SayMays <tanmkerusa05@gmail.com>
2025-03-22 03:45:07 +0500

    Добавлены файлы Web Lab 3

--- echoserver/echo/management/commands/manage_admin.py
+++ echoserver/echo/management/commands/manage_admin.py
@@ -2,14 +2,14 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User, Group
 
 class Command(BaseCommand):
-    help = "Управляет правами администратора: добавляет или удаляет пользователя из группы admin"
+    help = "Управляет ролью пользователя: заменяет текущую роль на admin или возвращает роль user"
 
     def add_arguments(self, parser):
         parser.add_argument('username', type=str, help='Имя пользователя')
         parser.add_argument(
             '--remove',
             action='store_true',
-            help='Если указано, удаляет пользователя из группы admin вместо добавления'
+            help='Если указано, убирает роль admin и возвращает роль user'
         )
 
     def handle(self, *args, **kwargs):
@@ -19,21 +19,25 @@ class Command(BaseCommand):
         try:
             user = User.objects.get(username=username)
             admin_group = Group.objects.get(name='admin')
+            user_group = Group.objects.get(name='user')
 
             if remove:
                 if admin_group in user.groups.all():
                     user.groups.remove(admin_group)
-                    self.stdout.write(self.style.SUCCESS(f'Пользователь {username} успешно удален из группы admin!'))
+                    if user_group not in user.groups.all():
+                        user.groups.add(user_group)
+                    self.stdout.write(self.style.SUCCESS(f'Роль пользователя {username} изменена на user!'))
                 else:
                     self.stdout.write(self.style.WARNING(f'Пользователь {username} не состоит в группе admin.'))
             else:
                 if admin_group not in user.groups.all():
+                    user.groups.clear()
                     user.groups.add(admin_group)
-                    self.stdout.write(self.style.SUCCESS(f'Пользователь {username} успешно добавлен в группу admin!'))
+                    self.stdout.write(self.style.SUCCESS(f'Роль пользователя {username} изменена на admin!'))
                 else:
-                    self.stdout.write(self.style.WARNING(f'Пользователь {username} уже состоит в группе admin.'))
+                    self.stdout.write(self.style.WARNING(f'Пользователь {username} уже имеет роль admin.'))
 
         except User.DoesNotExist:
             self.stdout.write(self.style.ERROR(f'Пользователь {username} не найден.'))
-        except Group.DoesNotExist:
-            self.stdout.write(self.style.ERROR('Группа admin не найдена.'))
+        except Group.DoesNotExist as e:
+            self.stdout.write(self.style.ERROR(f'Группа {e} не найдена.'))
